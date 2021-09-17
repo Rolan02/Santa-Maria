02abbd2f2782197a4696aa6bd4121905a1ee4562
Rolando Mamani Salas <Rolando.Mamani@fundacion-jala.org>
2021-09-17 16:07:25 -0400

    fist commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Maria_Micaela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
