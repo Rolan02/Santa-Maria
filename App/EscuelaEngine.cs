2b6d51aa31ccae170b06938ea0955460cbc63d9d
Rolando Mamani Salas <Rolando.Mamani@fundacion-jala.org>
2021-09-28 02:18:01 -0400

    Cargando datos con Linq

--- App/EscuelaEngine.cs
+++ App/EscuelaEngine.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Maria_Micaela.Entidades;
 
 namespace Maria_Micaela
@@ -14,6 +16,49 @@ namespace Maria_Micaela
         public void Inicializar()
         {
             Escuela = new Escuela("U.E. Santa Maria ", 1994, TiposEscuela.Primaria, ciudad: "Cochabamba", pais: "Bolivia");
+
+            CargarCursos();
+                
+            CargarAsignaturas();
+            CargarEvaluaciones();
+        }
+
+        private void CargarEvaluaciones()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CargarAsignaturas()
+        {
+            foreach (var curso in Escuela.Cursos)
+            {
+                List<Asignatura> listaAsignaturas = new List<Asignatura>()
+                {
+                    new Asignatura{Nombre = "Matematicas"},
+                    new Asignatura{Nombre = "Ingles"},
+                    new Asignatura{Nombre = "Lenguaje"},
+                    new Asignatura{Nombre = "Educacion Fisica"},
+                    new Asignatura{Nombre = "Sociales"},
+                };
+                curso.Asignaturas = (listaAsignaturas);
+            }
+        }
+
+        private List<Alumno> GenerarAlumnosAzar(int cantidad)
+        {
+            string[] nombre = {"rolando","daniel","carlos","rodrigo"};
+            string[] nombre2 = {"fulanito","Alambrito","Zoquete","Melendi"};
+            string[] apellido = {"quispe","mamani","salas","mendiza"};
+            //Lenguaje integrado de consultas =  Linq
+            var listaAlumnos = from n1 in nombre
+                                from n2 in nombre2
+                                from a1 in apellido
+                                select new Alumno{ Nombre = $"{n1} {n2} {a1}"};
+            return listaAlumnos.OrderBy(al => al.Id).Take(cantidad).ToList();
+        }
+
+        private void CargarCursos()
+        {
             Escuela.Cursos = new List<Curso>() {
                 new Curso(){Nombre = "101" , Jornada = TiposJornada.Manana },
                 new Curso(){Nombre = "201" , Jornada = TiposJornada.Manana},
@@ -21,6 +66,12 @@ namespace Maria_Micaela
                 new Curso(){Nombre = "401" , Jornada = TiposJornada.Manana },
                 new Curso(){Nombre = "501" , Jornada = TiposJornada.Manana},
             };
+            Random rnd =  new Random();
+            foreach (var cusos in Escuela.Cursos)
+            {
+                int cantRandom = rnd.Next(5,20);
+                cusos.Alumnos = GenerarAlumnosAzar(cantRandom);
+            }
         }
     }
 }
